45b3a0791c20cb45625f6b2ceb2e6630f9aa1850
Eric Edwards <eric.edwards02@gmail.com>
2024-03-04 12:40:53 -0800

    Initial commit

---
+++ tstat-web/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="App-header">
+        <h1 className="header-text">tstat</h1>
+        <p className="subheader-text">a statistical analysis tool for tdx</p>
+      </header>
+
+      <footer className="App-footer">
+        <p>c.o. Alex JPS and Eric Edwards @ UO Classrooms</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
