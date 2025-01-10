d49b7b706b07eabc7fadaea8f4389928f760a40b
Eric Edwards <eric.edwards02@gmail.com>
2025-01-09 16:28:36 -0800

    feat: Styled Nav component and margins.

--- src/App.jsx
+++ src/App.jsx
@@ -10,16 +10,17 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 
 // Routing
 import AuthProvider from "./provider/AuthProvider";
 import Routes from "./routes/index";
+
 // Components
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
+
 // Styles
 import "./App.css";
 
@@ -27,18 +28,21 @@ function App() {
   /* This component serves as the root of the app.
    */
   useEffect(() => {
-    document.body.classList.add("gray-background");
+    document.body.classList.add("white-background");
   }, []);
   return (
     <div className="App">
-      <Header />
-      <Nav />
-      <div className="App-body">
-        <AuthProvider>
-          <Routes />
-        </AuthProvider>
+      <div className="App-content">
+        <Header />
+        <Nav />
+        <div className="App-body">
+          <AuthProvider>
+            <Routes />
+          </AuthProvider>
+        </div>
+
+        <Footer />
       </div>
-      <Footer />
     </div>
   );
 }
