8f26b1eb6076f3c264a3759ace419bcd6e9bd869
Eric Edwards <eric.edwards02@gmail.com>
2025-02-05 11:20:18 -0800

    chore: Wrapped ticket fetch in useEffect hook when the filter is modified upon form submission.

--- frontend/src/pages/ViewCreationPage.tsx
+++ frontend/src/pages/ViewCreationPage.tsx
@@ -1,15 +1,41 @@
 // import GeneratedView from "../components/GeneratedView";
 import ReportCacheStatusCallout from "../components/ReportCacheStatusCallout";
 import ViewCreationForm from "../components/ViewCreationForm";
+import { useEffect, useState } from "react";
+import { IFormInputs } from "../components/ViewCreationForm";
+import { useAuth } from "../provider/AuthProvider";
 
 // A layout defines a set of parameters
 function ViewCreationPage() {
+  const [filter, setFilter] = useState({
+    grouping: "none",
+    layout: "chart",
+  } as IFormInputs);
+
+  const { token } = useAuth();
+
+  useEffect(() => {
+    fetch(
+      `${import.meta.env.VITE_API_URL}/api/tickets/fetch-filtered-tickets`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ filter: filter }),
+      },
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, [token, filter]);
+
   return (
     // <GeneratedView config={config} data={data} />
     <>
-        <h3>Create a new view</h3>
-        <ReportCacheStatusCallout />
-        <ViewCreationForm />
+      <h3>Create a new view</h3>
+      <ReportCacheStatusCallout />
+      <ViewCreationForm setFilter={setFilter} />
     </>
   );
 }
