1e9406f354f5906a9a414e47c30b353b77f27195
Eric Edwards <eric.edwards02@gmail.com>
2025-03-04 09:21:59 -0800

    fix: log out gracefully
    We no longer use a conditional useEffect, so no hooks are ever missing.

--- frontend/src/pages/LogoutPage.tsx
+++ frontend/src/pages/LogoutPage.tsx
@@ -1,53 +1,48 @@
 /*
  * Logout Page
- * by Alex JPS
- * 2024-05-02
  *
  * Defines the logout page, which just
  * removes the user's token and redirects them.
  */
 
 // Packages
-import {Navigate, useNavigate} from "react-router-dom";
-
-// Styles
-
-import {useAuth} from "../provider/AuthProvider";
-import {useEffect} from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../provider/AuthProvider";
+import { useEffect } from "react";
 
 function LogoutPage() {
-	/* This component displays the logout page.
-	 */
-	const { token, setToken} = useAuth();
-	const navigate = useNavigate();
+  /* This component displays the logout page.
+   */
+  const { token, setToken } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const handleLogout = () => {
+      setToken("");
+      navigate("/", { replace: true });
+      console.log("Logged out.");
+    };
 
     if (!token) {
-		// kick users who are not authenticated
-		return <Navigate to={"/invalid"} />;
+      // kick users who are not authenticated
+      navigate("/invalid", { replace: true });
+      return;
     }
 
-	useEffect(() => {
-		const handleLogout = () => {
-			setToken("");
-			navigate("/");
-		}
+    const logoutTimer = setTimeout(handleLogout, 3000);
 
-		if (token) {
-			setTimeout(() => {
-				handleLogout();
-			}, 3000);
-		}
-	}, [])
+    return () => clearTimeout(logoutTimer);
+  }, []);
 
-	return (
-		<div>
-			<h1>Logout Page</h1>
-			<p>
-				This is the logout page, which should only be visible to authenticated
-				users and immediately redirects them to the landing page.
-			</p>
-		</div>
-	);
+  return (
+    <div>
+      <h1>Logout Page</h1>
+      <p>
+        This is the logout page, which should only be visible to authenticated
+        users and immediately redirects them to the landing page.
+      </p>
+    </div>
+  );
 }
 
 export default LogoutPage;
