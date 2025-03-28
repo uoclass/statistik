b3c459d9370062043867de0e06bd16289897543a
Alex João Peterson Santos <git@alexjps.com>
2025-03-28 08:57:59 -0700

    feat: Show ticket counts in sections and copied text

--- frontend/src/components/TicketList.tsx
+++ frontend/src/components/TicketList.tsx
@@ -24,16 +24,19 @@ const TicketListSection = (
   return (
     <div className={`${(isOpen) ? "pb-2" : "pb-0"} mb-4 bg-neutral-100 px-4`}>
       <button className="flex flex-row justify-between w-full text-lg font-semibold mb-2 pt-3 pb-2" onClick={toggleOpen}>
-        <h3>{(!group_name) ? "Unspecified Group" : group_name}</h3>
+        <h3>
+          {(!group_name) ? "Unspecified Group" : group_name} 
+          <span className="text-neutral-500"> ({ticket_data.length})</span>
+        </h3>
         {isOpen ? <ChevronUp size={36} /> : <ChevronDown size={36} />}
       </button>
       {!isOpen ? null : <>
         <button
           className="px-3 flex items-center py-1 text-neutral-500 hover:text-neutral-600"
           onClick={() => {
-            let to_clipboard = "";
+            let to_clipboard = `${ticket_data.length} tickets matching given filters in section "${group_name}"`;
             for (let i = 0; i < ticket_data.length; i++) {
-              to_clipboard += i != 0 ? "\n-----\n" : "";
+              to_clipboard += "\n-----\n";
               to_clipboard += get_ticket_info_string(ticket_data[i]);
             }
             navigator.clipboard.writeText(to_clipboard);
